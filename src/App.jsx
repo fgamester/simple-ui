685e20fc461f822c61f2af8b492ac85d653d86dc
fgamester <falvarez.munoz@hotmail.es>
2024-12-11 19:06:41 -0300

    modal settings started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useCallback } from 'react'
 import simpleUI from './simple-ui/simple-ui'
 
 function App() {
 
+  const showModal = useCallback(() => {
+    simpleUI.modal({
+      title : 'Title',
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, repellendus nostrum eius corrupti minima voluptate?"
+    });
+  });
+
   return (
-      <div>
-        <button onClick={simpleUI.modal}>activate modal</button>
-      </div>
+    <div>
+      <button onClick={showModal}>activate modal</button>
+    </div>
   )
 }
 
--- src/simple-ui/simple-ui.js
+++ src/simple-ui/simple-ui.js
@@ -1,21 +1,46 @@
 import '../simple-ui/simple-ui.css'
 
 const simpleUI = {
-    modal: () => {
+    /**
+     * Creates a modal with the following elements: Title, text and close button.
+     * @param {Object} options - The options for the modal.
+     * @param {string} options.title - The title of the modal.
+     * @param {string} options.text - The text of the modal.
+     * @param {'center'|'top-left'|'top-center'|'top-right'|'center-left'|'center-right'|'bottom-left'|'bottom-center'|'bottom-right'} [options.position="center"] - The position of the modal.
+     */
+    modal: ({
+        title,
+        text,
+        position = 'center',
+    } = {}) => {
+
+        if (typeof title !== 'string') {
+            throw new TypeError("Title must be a string");
+        }
+
+        if (typeof text !== 'string') {
+            throw new TypeError("Text must be a string");
+        }
+
         const modal = document.createElement('div');
         const body = document.createElement('div');
-        const h1Comp = document.createElement('div');
-        const h3Comp = document.createElement('div');
+        const h2Comp = document.createElement('h2');
+        const h3Comp = document.createElement('h3');
         const closeBtn = document.createElement('button');
 
-        closeBtn.textContent = 'Close'
+        h2Comp.textContent = title;
+        h3Comp.textContent = text;
+        closeBtn.textContent = 'Close';
 
         modal.appendChild(body);
-        body.appendChild(h1Comp);
+        body.appendChild(h2Comp);
         body.appendChild(h3Comp);
         body.appendChild(closeBtn);
 
         modal.id = 'simple-modal';
+        modal.classList.add(`sui-modal-position-${position}`);
+        closeBtn.id = 'sui-btn-basic';
+
         document.body.appendChild(modal);
         closeBtn.addEventListener('click', () => document.body.removeChild(modal));
     }
